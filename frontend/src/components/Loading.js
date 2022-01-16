2f2b3c6f58b189c7a4c10c91516eb92990161437
jayo78 <jhurst1@terpmail.umd.edu>
2022-01-15 20:50:35 -0500

    work

--- frontend/src/components/Loading.js
+++ frontend/src/components/Loading.js
@@ -3,11 +3,9 @@ import { Box, CircularProgress, Text } from '@chakra-ui/react';
 
 const Loading = () => {
     return (
-        <Box position="relative" width="100%" height="400">
-            <Box position="absolute" top="50%" left="50%">
-                <CircularProgress isIndeterminate size="50px" color="primary" />
-                <Text>Loading</Text>
-            </Box>
+        <Box>
+            <CircularProgress isIndeterminate size="50px" color="primary" />
+            <Text>Loading</Text>
         </Box>
     );
 };
