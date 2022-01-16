65fdb6319d7abf0d019230b95897b32d7c0055a7
jayo78 <jhurst1@terpmail.umd.edu>
2022-01-15 21:28:33 -0500

    fix double join bug

--- backend/controllers/roomController.js
+++ backend/controllers/roomController.js
@@ -1,6 +1,6 @@
-const User = require('../models/User');
-const Participant = require('../models/Participant');
-const Room = require('../models/Room');
+const User = require("../models/User");
+const Participant = require("../models/Participant");
+const Room = require("../models/Room");
 const {
     addRoom,
     addParticipant,
@@ -9,63 +9,63 @@ const {
     getOpenRoom,
     removeRoom,
     removeParticipant,
-    participantInRoom
-} = require('../rooms');
+    participantInRoom,
+} = require("../rooms");
 
 /*
  * create room is only hit by users and not used when the backend itself is spinning up rooms.
  * rooms created by users should always be private
  * */
 const createRoom = async (req, res) => {
-    console.log('[roomController] createRoom');
+    console.log("[roomController] createRoom");
     let { userID } = req.body;
 
     //roomID
     // validate
     const userExists = await User.findOne({ _id: userID });
     if (!userExists) {
-        console.error('\tcreateRoom: user not found');
-        return res.status(400).json({ message: 'user not found' });
+        console.error("\tcreateRoom: user not found");
+        return res.status(400).json({ message: "user not found" });
     }
 
     //
     // create the room, add to map, and then return it
     Room.create({
         creator_id: userID,
-        type: 'private'
+        type: "private",
     }).then((newRoom) => {
-        console.log('\troom created: ' + newRoom._id);
+        console.log("\troom created: " + newRoom._id);
         addRoom(newRoom._id.toString());
         return res.status(201).json({
             _id: newRoom._id.toString(),
             creator_id: newRoom.creator_id,
-            type: newRoom.type
+            type: newRoom.type,
         });
     });
 };
 
 // can create anon participant or reference an authed user
 const createParticipant = async (req, res) => {
-    console.log('[roomController] createParticipant');
+    console.log("[roomController] createParticipant");
     let { roomID, userID, name } = req.body;
 
     // validate
     if (!roomExists(roomID)) {
-        console.error('\troom not found');
-        return res.status(400).json({ message: 'room not found' });
+        console.error("\troom not found");
+        return res.status(400).json({ message: "room not found" });
     }
 
     if (!roomHasSpace(roomID)) {
-        console.error('\troom full');
-        return res.status(400).json({ message: 'room full' });
+        console.error("\troom full");
+        return res.status(400).json({ message: "room full" });
     }
 
     if (userID) {
         // only check user if it is provided
         const userExists = await User.findOne({ _id: userID });
         if (!userExists) {
-            console.error('\tuser not found');
-            return res.status(400).json({ message: 'user not found' });
+            console.error("\tuser not found");
+            return res.status(400).json({ message: "user not found" });
         }
     }
 
@@ -75,57 +75,58 @@ const createParticipant = async (req, res) => {
         name: name,
         anon: userID == null,
         roomID: roomID,
-        userID: userID
+        userID: userID,
     }).then((newParticipant) => {
-        console.log('\tparticipant created: ' + newParticipant._id);
-        addParticipant(roomID, newParticipant._id);
+        console.log("\tparticipant created: " + newParticipant._id);
+        addParticipant(roomID, newParticipant._id.toString());
         return res.status(201).json({
             _id: newParticipant._id.toString(),
             name: newParticipant.name,
             anon: newParticipant.anon,
             roomID: newParticipant.roomID,
-            userID: newParticipant.userID
+            userID: newParticipant.userID,
         });
     });
 };
 
 // find an open room or create a new one
 const findRoom = async (req, res) => {
-    console.log('[roomController] findRoom');
+    console.log("[roomController] findRoom");
 
     let roomID = getOpenRoom();
     if (!roomID) {
         // no open rooms
         Room.create({
             creator_id: null,
-            type: 'public'
+            type: "public",
         }).then((newRoom) => {
-            console.log('\troom created: ' + newRoom._id);
+            console.log("\troom created: " + newRoom._id);
             addRoom(newRoom._id.toString());
             return res.status(201).json({
-                roomID: newRoom._id.toString()
+                roomID: newRoom._id.toString(),
             });
         });
-    } else {    //
+    } else {
+        //
         return res.status(201).json({
-            roomID: roomID
+            roomID: roomID,
         });
     }
 };
 
 const getMessages = async (req, res) => {
-    console.log('[roomController] getMessages');
+    console.log("[roomController] getMessages");
 
     let roomID = req.params.roomID;
 
-    let room = await Room.findOne({_id: roomID});
+    let room = await Room.findOne({ _id: roomID });
     if (!room) {
-        console.log('getMessages failed, unrecognized roomID');
+        console.log("getMessages failed, unrecognized roomID");
         return res.status(404);
     }
 
     return res.status(200).json({
-        messages: room.messages
+        messages: room.messages,
     });
 };
 
@@ -133,5 +134,5 @@ module.exports = {
     createRoom,
     createParticipant,
     findRoom,
-    getMessages
+    getMessages,
 };
